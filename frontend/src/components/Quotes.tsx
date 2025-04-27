5abc1bff5cadc1b07991cd94d690c0ba3f2c0f44
Yash Vishnoi <yashvishnoi01.yv@gmail.com>
2025-04-27 22:13:52 +0530

    My bloging website-2

---
+++ frontend/src/components/Quotes.tsx
@@ -0,0 +1,24 @@
+
+export const Quote = () => {
+ return < div className="bg-slate-200 h-screen flex justify-center flex-col">
+    <div className="flex justify-center">
+
+      <div className="max-w-lg">
+
+        <div className="text-3xl font-bold">
+        The Customer Service I received was Exceptional. I would highly recommend this company to anyone looking for a new website.
+        </div>
+
+        <div className="max-w-md text-xl font-semibold mt-4">
+          Yash Vishnoi
+        </div>
+        <div className="max-w-md text-sm font-light text-slate-400">
+          Coder | Web Developer
+        </div>  
+      
+      </div>
+
+    </div> 
+   
+  </div>
+} 
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL="https://backend.yashpco2020.workers.dev"
